c10b1663f03d8b043f91db8a2f9aafe3642c42a5
Joe Polo <joe.polo@telus.com>
2023-01-11 17:28:36 -0800

    create error handling component

--- stories/index.js
+++ stories/index.js
@@ -15,7 +15,8 @@ import Header from 'components/Appointment/Header.js';
 import Empty from 'components/Appointment/Empty.js';
 import Show from "components/Appointment/Show.js";
 import Confirm from 'components/Appointment/Confirm.js';
-
+import Status from 'components/Appointment/Status.js';
+import Error from 'components/Appointment/Error.js';
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,4 +149,6 @@ storiesOf("InterviewerList", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => <Show student="Mickey Mouse" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
-  .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />);
+  .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Status", ()  => <Status message="Deleting" />)
+  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)

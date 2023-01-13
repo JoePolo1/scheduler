78e9fcdd7bc733e596921a39e528386eba3e1957
Joe Polo <joe.polo@telus.com>
2023-01-13 14:59:41 -0800

    complete useVisualMode functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,45 @@
 import { useState } from "react";
 
 // Function to use visual mode takes in an initial state
-export default function useVisualMode(initialMode)  {
+export default function useVisualMode(initial)  {
 
   // it sets useState to the initial mode provided/passed down to it
-  const [modeState, setModeState] = useState(initialMode);
+  // const [modeState, setModeState] = useState(initialMode);
+  const [history, setHistory] = useState([initial]);
 
   // It returns an object with the mode property
-  return { 
-    mode: modeState }
+  // return { 
+  //   mode: modeState }
+
+  // Function which transitions the state if a new mode is provided
+  function transition(mode, replace = false) {
+
+    // Ternary conditional which, if replace is true, keeps the entire history stack minus the most recent and updates the mode.
+    // Otherwise, updates the mode to the history stack
+    setHistory((prev) => replace ? [...prev.slice(0, -1), mode] : [...prev, mode])
+
+  }
+
+  // Transitions to previous history stack mode, unless there is none (first page visited in a browser with nothing to go back to)
+  function back() {
+
+    setHistory((prev) => prev.length > 1 ? prev.slice(0, -1) : prev)
+
+  }
+
+  // Returns the new modes
+  return {
+    mode: history[history.length -1],
+    transition, 
+    back
+  }
+
 };
+
+
+// [
+//   show,
+//   save,
+//   create,
+//   empty,
+// ]

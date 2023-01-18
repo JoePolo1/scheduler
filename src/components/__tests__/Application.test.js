96825462dce9174b2b2c2bd272aba30246dfc2d1
Joe Polo <joe.polo@telus.com>
2023-01-17 23:48:57 -0800

    continue integration testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,18 +1,72 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, getByAltText, getByPlaceholderText, getAllByTestId, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
 
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  it("Changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+
+
+  // it("Loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  //   const { container } = render(<Application />);
+
+  //   await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  //   const appointments = getAllByTestId(container, "appointment");
+  //   console.log(prettyDOM(appointments));
+
+  //   const appointment = getAllByTestId(container, "appointment")[0];
+  //   console.log(prettyDOM(appointment));
+
+  //   // Click the add button on the first empty appointment
+  //   fireEvent.click(getByAltText(appointment, "Add"));
+
+  //   // Click on the input field for tyhe student name
+  //   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+  //     target: { value: "Lydia Miller-Jones" }
+  //   });
+  //   // Add a new name in the student name input
+  //   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  //   // Click on save
+  //   fireEvent.click(getByText(appointment, "Save"));
+
+  //   console.log(prettyDOM(container));
+  // })
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
     });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    console.log(prettyDOM(appointment));
+  });
+
 });
+
 
